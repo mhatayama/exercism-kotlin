3e2803433e3603046daf7a92a25607267b628b4a
Masato Hatayama <masato.hatayama@gmail.com>
2020-05-22 15:52:34 +0900

    submit

--- pig-latin/src/main/kotlin/PigLatin.kt
+++ pig-latin/src/main/kotlin/PigLatin.kt
@@ -2,16 +2,15 @@ object PigLatin {
     val consonants = "bcdfghjklmnpqrstvwxyz"
 
     fun translate(phrase: String): String {
-        return phrase.split(" ").map { piggify(it) }.joinToString(" ")
+        return phrase.split(" ").joinToString(" ") { piggify(it) }
     }
 
     private fun piggify(word: String): String = when {
-        Regex("""^[aeiou]|xr|yt""").containsMatchIn(word) -> word + "ay"
-        Regex("""^ch|qu""").containsMatchIn(word) -> word.drop(2) + word.take(2) + "ay"
-        Regex("""^sch""").containsMatchIn(word) -> word.drop(3) + word.take(3) + "ay"
-        Regex("""^[$consonants]qu""").containsMatchIn(word) -> word.drop(3) + word.take(3) + "ay"
-        Regex("""^[$consonants]y""").containsMatchIn(word) -> word.drop(2) + word.take(2) + "ay"
-        Regex("""^[$consonants]""").containsMatchIn(word) -> word.drop(1) + word.first() + "ay"
+        Regex("""^[aeiou]|^xr|^yt""").containsMatchIn(word) -> word + "ay"
+        Regex("""^sch|^thr|^squ""").containsMatchIn(word) -> word.drop(3) + word.take(3) + "ay"
+        Regex("""^ch|^qu|^rh|^th""").containsMatchIn(word) -> word.drop(2) + word.take(2) + "ay"
+        Regex("""^[$consonants]y""").containsMatchIn(word) -> word.drop(1) + word.take(1) + "ay"
+        Regex("""^[$consonants]""").containsMatchIn(word) -> word.drop(1) + word.take(1) + "ay"
         else -> word
     }
 }
