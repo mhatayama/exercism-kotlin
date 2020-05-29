c392c4fe3d7a3a74458d0e718ddf01ec4680cd04
Masato Hayayama <masato.hatayama@gmail.com>
2020-05-29 19:52:18 +0900

    use map

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,9 +1,19 @@
 object ScrabbleScore {
-    private val VALUES = listOf(1 to "AEIOULNRST", 2 to "DG", 3 to "BCMP",
-            4 to "FHVWY", 5 to "K", 8 to "JX", 10 to "QZ")
+    private val letterValues: Map<Char, Int> = mapOf(
+            *('A'..'Z').map {
+                it to when {
+                    "AEIOULNRST".contains(it) -> 1
+                    "DG".contains(it) -> 2
+                    "BCMP".contains(it) -> 3
+                    "FHVWY".contains(it) -> 4
+                    "K".contains(it) -> 5
+                    "JX".contains(it) -> 8
+                    "QZ".contains(it) -> 10
+                    else -> 0
+                }
+            }.toTypedArray())
 
-    private fun scoreLetter(c: Char): Int =
-            VALUES.first { it.second.contains(c) }.first
+    private fun scoreLetter(c: Char): Int = letterValues.getValue(c)
 
     fun scoreWord(word: String): Int =
             word.sumBy { scoreLetter(it.toUpperCase()) }
