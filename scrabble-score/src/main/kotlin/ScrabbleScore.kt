294bc43e30ee8910a3535c881919a542157bed8d
Masato Hayayama <masato.hatayama@gmail.com>
2020-05-30 08:31:39 +0900

    better solution

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,17 +1,8 @@
 object ScrabbleScore {
     private val letterValues: Map<Char, Int> = mapOf(
-            *('A'..'Z').map {
-                it to when {
-                    "AEIOULNRST".contains(it) -> 1
-                    "DG".contains(it) -> 2
-                    "BCMP".contains(it) -> 3
-                    "FHVWY".contains(it) -> 4
-                    "K".contains(it) -> 5
-                    "JX".contains(it) -> 8
-                    "QZ".contains(it) -> 10
-                    else -> 0
-                }
-            }.toTypedArray())
+            "AEIOULNRST" to 1, "DG" to 2, "BCMP" to 3,
+            "FHVWY" to 4, "K" to 5, "JX" to 8, "QZ" to 10
+    ).flatMap { (letters, score) -> letters.map { it to score } }.toMap()
 
     private fun scoreLetter(c: Char): Int = letterValues.getValue(c)
 
