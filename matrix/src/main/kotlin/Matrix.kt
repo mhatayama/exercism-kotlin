8eca0f3e7fb452ca512b1dea928cca0c7101b035
Masato Hayayama <masato.hatayama@gmail.com>
2020-06-12 04:37:49 +0900

    more simpler solution

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -2,8 +2,7 @@ class Matrix(private val matrixAsString: String) {
     private val spaceRegex = """\s+""".toRegex()
     private val matrix: List<List<Int>> = matrixAsString.lines()
             .map { it.trim().split(spaceRegex).map(String::toInt) }
-    private val rowSize = matrixAsString.lines().size
 
-    fun column(colNr: Int): List<Int> = (0 until rowSize).map { matrix[it][colNr - 1] }
+    fun column(colNr: Int): List<Int> = matrix.map { it[colNr - 1] }
     fun row(rowNr: Int): List<Int> = matrix[rowNr - 1]
 }
