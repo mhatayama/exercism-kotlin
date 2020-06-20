284c2c28a824c95efb4d5b1b43779b8ef180482c
Masato Hayayama <masato.hatayama@gmail.com>
2020-06-20 15:51:58 +0900

    submit

--- bob/src/main/kotlin/Bob.kt
+++ bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,15 @@
 object Bob {
     fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
+        val trimmed = input.trim()
+        val isYelling = trimmed.contains(Regex("""[A-Z]""")) and !trimmed.contains(Regex("""[a-z]"""))
+        val isQuestioning = trimmed.endsWith("?")
+        return when {
+            trimmed.isEmpty() -> "Fine. Be that way!"
+            isYelling and isQuestioning -> "Calm down, I know what I'm doing!"
+            isYelling -> "Whoa, chill out!"
+            isQuestioning -> "Sure."
+            else -> "Whatever."
+        }
     }
 }
+
